9070b9fbba23125cc2b983d92f406ea1f360f261
Devender <bishtdev293@gmail.com>
2025-08-28 22:26:13 +0530

    feat: Create the header for the game.
    Create the header with the name and a select to select the level for the game.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [level, setLevel] = useState("easy");
+
+  const changeLevel = (event)=> {
+    setLevel(event.target.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className='header'>
+        <h1><span>Poke</span><span>Game</span></h1>
+        <div className='level'>
+          <select id="level" name="level" value={level} onChange={(event)=> changeLevel(event)}>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </div>
+      </section>
     </>
   )
 }
