a397c7248241833ced78bed1e3a7399825dd2f89
Devender <bishtdev293@gmail.com>
2025-08-29 18:40:57 +0530

    feat: Add the score and best score for the game.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 
 import './App.css'
@@ -5,6 +6,8 @@ import './App.css'
 function App() {
 
   const [level, setLevel] = useState("easy");
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   const changeLevel = (event)=> {
     setLevel(event.target.value);
@@ -13,7 +16,7 @@ function App() {
   return (
     <>
       <section className='header'>
-        <h1><span>Poke</span><span>Game</span></h1>
+        <h1><span className='color-red'>Poke</span><span>Game</span></h1>
         <div className='level'>
           <select id="level" name="level" value={level} onChange={(event)=> changeLevel(event)}>
             <option value="easy">Easy</option>
@@ -21,6 +24,11 @@ function App() {
             <option value="hard">Hard</option>
           </select>
         </div>
+      </section>
+
+      <section className='score-card'>
+        <div><span className='color-red'>Score:</span>  {score}</div>
+        <div><span className='color-red'>Best Score:</span> {bestScore} </div>
       </section>
     </>
   )
